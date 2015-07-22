9bb863d04562668c894c3d015073a6253fc462ab
Jorgen Modin <jorgen@webworks.se>
2015-07-22 12:32:08 +0200

    Add regtest network support to bitcore

--- app/master/network.js
+++ app/master/network.js
@@ -14,6 +14,20 @@ var errors = require('../../lib/errors')
 var logger = require('../../lib/logger').logger
 var util = require('../../lib/util')
 
+// add regtest network support to bitcore
+bitcore.Networks.add({
+  name: 'regtest',
+  alias: 'regtest',
+  pubkeyhash: 0x6f,
+  privatekey: 0xef,
+  scripthash: 0xc4,
+  xpubkey: 0x043587cf,
+  xprivkey: 0x04358394,
+  networkMagic: 0xFABFB5DA,
+  port: 8333,
+  dnsSeeds: [ ]
+});
+
 /**
  * @event Network#block
  * @param {string} hash
